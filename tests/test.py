5fbfcaeb7bbc6cfaef3a55f324d65342b725552b
adamjpatterson <adam@farar.net>
2024-10-20 23:24:09 -0400

    Improve tests.

--- tests/test.py
+++ tests/test.py
@@ -26,30 +26,36 @@ def callable1(arg0: Any) -> Any:
 class TestAll(unittest.TestCase):
 
     def test_cache_member(self) -> None:
+        cache.clear_all()
         identity({"a": 42})
         self.assertIn(identity, cache._cache)
 
     def test_cache_callable_member(self) -> None:
+        cache.clear_all()
         identity({"a": 42})
         self.assertIn((((("a", 42),),), ()), cache._cache[identity])
 
     def test_callstack_deadlock(self) -> None:
+        cache.clear_all()
         result = callable1(42)
         self.assertEqual(((42,), {}), result)
 
     def test_cicular_reference(self) -> None:
+        cache.clear_all()
         x = []
         x.append(x)
         identity(x)
         self.assertEqual(cache._cache[identity][(((...,),), ())], (([x],), {}))
 
     def test_cache_entry_removal(self) -> None:
+        cache.clear_all()
         identity({"a": 42}, a=42)
         identity({"a": 23}, a=23)
         cache.clear(identity, {"a": 42}, a=42)
         self.assertEqual(cache._cache, {identity: {(((("a", 23),),), ((("a", 23),))): (({"a": 23},), {"a": 23})}})
 
     def test_cache_callable_removal(self) -> None:
+        cache.clear_all()
         identity({"a": 42})
         cache.clear(identity, {"a": 42})
         self.assertEqual(cache._cache, {})
